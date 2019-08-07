a16a2f9b3499fc6c5a5fba8ab2a253ec3bfbc832
esic <esic@192.168.1.51>
2019-08-07 13:22:03 +0200

    AJOUT CURRENT TIMESTAMP

--- src/fr/solutec/dao/PoidsDao.java
+++ src/fr/solutec/dao/PoidsDao.java
@@ -17,15 +17,27 @@ import java.util.List;
  * @author ESIC
  */
 public class PoidsDao {
-    public static Poids addPoid(String mail, String mdp) throws SQLException {
-        Poids p = null;
-        
-      
-        
-        
-        return p;
+    public static void addPoid (User user) throws SQLException {
+        String sql = "INSERT INTO ";
+                
 }
     
   }
        
+ public static void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO user (nomuser, prenomuser, mailuser, mdpuser, sexeuser, poidiniuser"
+                + "tailleuser,ageuser) VALUES(?,?,?,?,?,?,?,?)";
+        Connection connexion = AccessDAO.getConnection();
+        PreparedStatement ordre = connexion.prepareStatement(sql);
 
+        ordre.setString(1, user.getNom());
+        ordre.setString(2, user.getPrenom());
+        ordre.setString(3, user.getMail());
+        ordre.setString(4, user.getMdp());
+        ordre.setString(5, user.getSexe());
+        ordre.setDouble(6, user.getPoid());
+        ordre.setInt(7, user.getTaille());
+        ordre.setInt(8, user.getAge());
+
+        ordre.execute();
+    }
