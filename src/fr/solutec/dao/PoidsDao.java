a41aa726536a63f7ff9c9481fac2fb463a1a9b48
esic <esic@192.168.1.51>
2019-08-07 14:12:06 +0200

    DAO POIDS
    ADDPOIDS

--- src/fr/solutec/dao/PoidsDao.java
+++ src/fr/solutec/dao/PoidsDao.java
@@ -4,23 +4,94 @@
  * and open the template in the editor.
  */
 package fr.solutec.dao;
+
 import fr.solutec.model.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 /**
  *
  * @author ESIC
  */
 public class PoidsDao {
-    public static void addPoids (User user) throws SQLException {
-        String sql = "INSERT INTO ";
-                
+
+    public static void addPoids(User user, Poids poids) throws SQLException {
+        String sql = "INSERT INTO poid (valeurpoid,user_iduser) VALUES (?,?)";
+        Connection connexion = AccessDAO.getConnection();
+
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+
+        ordre.setDouble(1, poids.getPoids());
+        ordre.setInt(2, user.getId());
+
+    }
+
+    public static List<Poids> getMyPoids(User user) throws SQLException {
+        List<Poids> result = new ArrayList<>();
+        
+        
+        String sql = "SELECT * FROM poid WHERE id_iduser=?";
+        
+        Connection connexion = AccessDAO.getConnection();
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+        
+        ordre.setInt(1, user.getId());
+        
+        
+        
+        
+        
+
+        return result;
+    }
+
 }
-    
-  }
-       
+
+/*    public static List <User> getAll() throws SQLException{
+        
+    List <User>  result = new ArrayList<>();
+        
+        String sql = "SELECT* FROM personne";
+        
+        Connection connexion = AccessDao.getConnection();
+        
+        PreparedStatement requette = connexion.prepareStatement(sql); //vient de java sql
+        
+            
+        ResultSet rs = requette.executeQuery(); //on stock la réponse de la requette sql dans rs
+        
+        while(rs.next()){
+            User u = new User();
+            u.setId(rs.getInt("idpersonne"));
+            u.setNom(rs.getString("nom"));
+            u.setPrenom(rs.getString("prenom"));
+            u.setLogin(rs.getString("login"));
+            result.add(u);
+        }
+        return result;
+    }
+
+
+public static void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO user (nomuser, prenomuser, mailuser, mdpuser, sexeuser, poidiniuser,tailleuser,ageuser) VALUES(?,?,?,?,?,?,?,?)";
+        Connection connexion = AccessDAO.getConnection();
+        PreparedStatement ordre = connexion.prepareStatement(sql);
+
+        ordre.setString(1, user.getNom());
+        ordre.setString(2, user.getPrenom());
+        ordre.setString(3, user.getMail());
+        ordre.setString(4, user.getMdp());
+        ordre.setString(5, user.getSexe());
+        ordre.setDouble(6, user.getPoid());
+        ordre.setInt(7, user.getTaille());
+        ordre.setInt(8, user.getAge());
+
+        ordre.execute();
+    }*/
