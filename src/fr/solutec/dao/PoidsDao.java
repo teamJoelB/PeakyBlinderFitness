46a07113ab91f37eeaff5390706d8a8fcab096ec
esic <esic@192.168.1.51>
2019-08-07 12:16:16 +0200

    ajouts de la valeur du poids dans la classe poid et final user DAO

--- src/fr/solutec/dao/PoidsDao.java
+++ src/fr/solutec/dao/PoidsDao.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package fr.solutec.dao;
+import fr.solutec.model.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author ESIC
  */
 public class PoidsDao {
-    
+    public static Poids addPoid(String mail, String mdp) throws SQLException {
+        Poids p = null;
+        
+      
+        
+        
+        return p;
 }
+    
+  }
+       
+
