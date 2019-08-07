936eb4c996ea7158751823e6fa02dbc6060707a9
esic <esic@192.168.1.51>
2019-08-07 12:01:51 +0200

    AJOUT USER DAO

--- src/fr/solutec/dao/UserDao.java
+++ src/fr/solutec/dao/UserDao.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package fr.solutec.dao;
+
 import fr.solutec.model.*;
 
 import java.sql.Connection;
@@ -12,25 +13,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author ESIC
  */
 public class UserDao {
-    public static User getByLoginPass(String mail, String mdp)throws SQLException{
+
+    public static User getByLoginPass(String mail, String mdp) throws SQLException {
         User result = null;
         String sql = "SELECT* FROM user WHERE mailuser=?AND mdpuser=?";
-        
+
         Connection connexion = AccessDAO.getConnection();
-        
+
         PreparedStatement requette = connexion.prepareStatement(sql); //vient de java sql
-        
+
         requette.setString(1, mail);
         requette.setString(2, mdp);
-        
+
         ResultSet rs = requette.executeQuery(); //on stock la réponse de la requette sql dans rs
-        
-        if (rs.next()){
+
+        if (rs.next()) {
             result = new User();
             result.setId(rs.getInt("iduser"));
             result.setNom(rs.getString("nomuser"));
@@ -40,29 +43,57 @@ public class UserDao {
             result.setSexe(rs.getString("sexeuser"));
             result.setTaille(rs.getInt("tailleuser"));
             result.setPrenom(rs.getString("prenom"));
-            result.setMail(rs.getString("mailuser"));  
-            result.setMdp(rs.getString("mdpuser"));  
-            
+            result.setMail(rs.getString("mailuser"));
+            result.setMdp(rs.getString("mdpuser"));
+
         }
-        
+
         return result;
     }
-    
-        public static void addUser(User user) throws SQLException{
+
+    public static void addUser(User user) throws SQLException {
         String sql = "INSERT INTO user (nomuser, prenomuser, mailuser, mdpuser, sexeuser, poidiniuser"
                 + "tailleuser,ageusuer) VALUES(?,?,?,?,?,?,?,?)";
         Connection connexion = AccessDAO.getConnection();
         PreparedStatement ordre = connexion.prepareStatement(sql);
-        
+
         ordre.setString(1, user.getNom());
         ordre.setString(2, user.getPrenom());
         ordre.setString(3, user.getMail());
         ordre.setString(4, user.getMdp());
-        ordre.setString(5, user.getMdp());
-        ordre.setString(6, user.getMdp());
-        ordre.setString(7, user.getMdp());
-        ordre.setString(8, user.getMdp());
-             
-        ordre.execute();       
-}
+        ordre.setString(5, user.getSexe());
+        ordre.setDouble(6, user.getPoid());
+        ordre.setInt(7, user.getTaille());
+        ordre.setInt(8, user.getAge());
+
+        ordre.execute();
+    }
+
+    public static List<User> getAll() throws SQLException {
+
+        List<User> result = new ArrayList<>();
+
+        String sql = "SELECT* FROM personne";
+
+        Connection connexion = AccessDAO.getConnection();
+
+        PreparedStatement requette = connexion.prepareStatement(sql); //vient de java sql
+
+        ResultSet rs = requette.executeQuery(); //on stock la réponse de la requette sql dans rs
+
+        while (rs.next()) {
+            User u = new User();
+            u.setId(rs.getInt("iduser"));
+            u.setNom(rs.getString("nomuser"));
+            u.setPrenom(rs.getString("prenomuser"));
+            u.setMail(rs.getString("mailuser"));
+            u.setSexe(rs.getString("sexeuser"));
+            u.setTaille(rs.getInt("tailleuser"));
+            u.setPoid(rs.getDouble("poidiniuser"));
+            u.setAge(rs.getInt("ageuser"));           
+            
+            result.add(u);
+        }
+        return result;
+    }
 }
